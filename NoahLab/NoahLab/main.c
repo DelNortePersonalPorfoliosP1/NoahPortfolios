23faa38c038b2760719105feef23743d34e21725
noah ahooja <noah.ahooja@gmail.com>
2019-09-18 08:42:42 -0700

    replaced main.c wiht pythag lab
    replaced main.c wiht pythag lab

---
+++ NoahLab/NoahLab/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  NoahLab
+//
+//  Created by Ahooja, Noah on 9/18/19.
+//  Copyright © 2019 Ahooja, Noah. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
